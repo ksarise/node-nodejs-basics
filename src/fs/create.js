9c80690747620b6b4df8c00b3d1802f65b1e677b
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-10-07 06:40:11 +0700

    feat: finish fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+import url from "node:url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "fresh.txt");
+
 const create = async () => {
-    // Write your code here 
+  await fs.writeFile(filePath, "I am fresh and young", (err) =>
+    console.error("FS operation failed", err)
+  );
 };
 
 await create();
