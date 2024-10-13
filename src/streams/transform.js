d5408bf5232a273915a8ab6a76fd92698be15542
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-10-13 21:03:41 +0700

    feat: finish streams transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from "node:stream";
+
 const transform = async () => {
-    // Write your code here 
+  const stream = new Transform({
+    transform(chunk, encoding, callback) {
+      const reverse = chunk.toString().split("").reverse().join("");
+      callback(null, reverse);
+    },
+  });
+
+  process.stdin.pipe(stream).pipe(process.stdout);
 };
 
 await transform();
