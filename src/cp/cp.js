b41fd35ea1755ab00742222d2906951e75c9127d
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-10-13 21:33:35 +0700

    feat: finish cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import path from "node:path";
+import fs from "node:fs";
+import url from "node:url";
+import { spawn } from "node:child_process";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = spawn("node", [filePath, ...args]);
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
+
+  childProcess.on("error", (err) => {
+    console.error("Child process failed", err);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["1", "2"]);
