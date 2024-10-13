af77245c99ff556969c4886594c379413d4ed361
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-10-13 18:03:21 +0700

    feat: finish fs read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+import url from "node:url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  console.log(
+    await fs
+      .readFile(filePath, "utf-8")
+      .catch((error) => console.error("FS operation failed", error))
+  );
 };
 
 await read();
