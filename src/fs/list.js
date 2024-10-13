adee6d39b852ca1e41534921772433fe64d0490e
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-10-13 18:22:15 +0700

    feat: finish fs list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+import url from "node:url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files");
+
 const list = async () => {
-    // Write your code here 
+  await fs
+    .readdir(filePath)
+    .then(console.log)
+    .catch((error) => console.error("FS operation failed", error));
 };
 
 await list();
