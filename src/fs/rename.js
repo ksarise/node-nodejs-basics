9458b988c90b8c30f29bb7666cfde87f5361696e
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-10-13 20:32:07 +0700

    feat: finish fs rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+import url from "node:url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const wrongFilePath = path.join(__dirname, "files", "wrongFilename.txt");
+const propFilePath = path.join(__dirname, "files", "properFilename.md");
+
 const rename = async () => {
-    // Write your code here 
+  await fs.rename(wrongFilePath, propFilePath).catch((error) => {
+    console.error("FS operation failed", error);
+  });
 };
 
 await rename();
