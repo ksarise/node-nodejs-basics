77a5ed61270d935e6969d1ca97dbc696ebe91933
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-10-13 18:13:14 +0700

    feat: finish fs copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,18 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+import url from "node:url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files");
+
 const copy = async () => {
-    // Write your code here 
+  await fs
+    .cp(filePath, `${filePath}_copy`, {
+      recursive: true,
+      errorOnExist: true,
+      force: false,
+    })
+    .catch((error) => console.error("FS operation failed", error));
 };
 
 await copy();
