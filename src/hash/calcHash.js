07b6e66a38e5c312195b0c3c8717695924998228
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-10-13 20:42:12 +0700

    feat: finish calchash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import path from "node:path";
+import fs from "node:fs";
+import url from "node:url";
+import { createHash } from "node:crypto";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const stream = fs.createReadStream(filePath);
+  stream.pipe(hash).setEncoding("hex").pipe(process.stdout);
 };
 
 await calculateHash();
