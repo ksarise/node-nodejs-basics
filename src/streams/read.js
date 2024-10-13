4cceebd3b477a9edaa3af0a394baff8bcc683ec0
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-10-13 20:49:37 +0700

    feat: finish streams read and write

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import path from "node:path";
+import fs from "node:fs";
+import url from "node:url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  const stream = fs.createReadStream(filePath);
+  stream.pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import path from "node:path";
+import fs from "node:fs";
+import url from "node:url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "fileToWrite.txt");
+
 const write = async () => {
-    // Write your code here 
+  const stream = fs.createWriteStream(filePath);
+  process.stdin.pipe(stream);
+  process.on("SIGINT", () => {
+    process.exit();
+  });
 };
 
 await write();
